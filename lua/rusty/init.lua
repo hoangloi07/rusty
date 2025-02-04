7ee44988329d933bb8a6efb306dbe4139ce00cb5
Arman Nikoyan <armannikoyan04@icloud.com>
2025-02-04 19:00:30 +0400

    version 1.0.2

--- lua/rusty/init.lua
+++ lua/rusty/init.lua
@@ -33,34 +33,34 @@ local config = {
 
 M = {
 	normal = {
-		a = { fg = config.colors.background, bg = config.colors.blue, gui = "bold" },
-		b = { fg = config.colors.foreground, bg = config.colors.line },
-		c = { fg = config.colors.foreground, bg = config.colors.background },
+		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.blue, gui = "bold" },
+		b = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.line },
+		c = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.background },
 	},
 	insert = {
-		a = { fg = config.colors.background, bg = config.colors.green, gui = "bold" },
-		b = { fg = config.colors.foreground, bg = config.colors.line },
-		c = { fg = config.colors.foreground, bg = config.colors.background },
+		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.blue, gui = "bold" },
+		b = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.line },
+		c = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.background },
 	},
 	visual = {
-		a = { fg = config.colors.background, bg = config.colors.purple, gui = "bold" },
-		b = { fg = config.colors.foreground, bg = config.colors.line },
-		c = { fg = config.colors.foreground, bg = config.colors.background },
+		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.blue, gui = "bold" },
+		b = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.line },
+		c = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.background },
 	},
 	replace = {
-		a = { fg = config.colors.background, bg = config.colors.red, gui = "bold" },
-		b = { fg = config.colors.foreground, bg = config.colors.line },
-		c = { fg = config.colors.foreground, bg = config.colors.background },
+		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.blue, gui = "bold" },
+		b = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.line },
+		c = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.background },
 	},
 	command = {
-		a = { fg = config.colors.background, bg = config.colors.orange, gui = "bold" },
-		b = { fg = config.colors.foreground, bg = config.colors.line },
-		c = { fg = config.colors.foreground, bg = config.colors.background },
+		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.blue, gui = "bold" },
+		b = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.line },
+		c = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.background },
 	},
 	inactive = {
-		a = { fg = config.colors.foreground, bg = config.colors.line, gui = "bold" },
-		b = { fg = config.colors.foreground, bg = config.colors.line },
-		c = { fg = config.colors.foreground, bg = config.colors.background },
+		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.blue, gui = "bold" },
+		b = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.line },
+		c = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.background },
 	},
 }
 
