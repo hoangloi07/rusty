202752062b319316b99b656a96c76f45296ccf5c
Arman Nikoyan <armannikoyan04@icloud.com>
2025-02-04 18:51:18 +0400

    lualine combined

--- lua/rusty/init.lua
+++ lua/rusty/init.lua
@@ -31,6 +31,39 @@ local config = {
 	}
 }
 
+M = {
+	normal = {
+		a = { fg = colors.background, bg = colors.blue, gui = "bold" },
+		b = { fg = colors.foreground, bg = colors.line },
+		c = { fg = colors.foreground, bg = colors.background },
+	},
+	insert = {
+		a = { fg = colors.background, bg = colors.green, gui = "bold" },
+		b = { fg = colors.foreground, bg = colors.line },
+		c = { fg = colors.foreground, bg = colors.background },
+	},
+	visual = {
+		a = { fg = colors.background, bg = colors.purple, gui = "bold" },
+		b = { fg = colors.foreground, bg = colors.line },
+		c = { fg = colors.foreground, bg = colors.background },
+	},
+	replace = {
+		a = { fg = colors.background, bg = colors.red, gui = "bold" },
+		b = { fg = colors.foreground, bg = colors.line },
+		c = { fg = colors.foreground, bg = colors.background },
+	},
+	command = {
+		a = { fg = colors.background, bg = colors.orange, gui = "bold" },
+		b = { fg = colors.foreground, bg = colors.line },
+		c = { fg = colors.foreground, bg = colors.background },
+	},
+	inactive = {
+		a = { fg = colors.foreground, bg = colors.line, gui = "bold" },
+		b = { fg = colors.foreground, bg = colors.line },
+		c = { fg = colors.foreground, bg = colors.background },
+	},
+}
+
 -- Setup user configuration
 function M.setup(user_config)
 	config = vim.tbl_deep_extend("force", config, user_config or {})
