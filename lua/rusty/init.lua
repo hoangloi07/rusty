40d12457ac442f45a6e21d52a8e693b0cdfe8ce1
Arman Nikoyan <armannikoyan04@icloud.com>
2025-02-04 19:10:29 +0400

    minor change

--- lua/rusty/init.lua
+++ lua/rusty/init.lua
@@ -38,27 +38,27 @@ local lualine = {
 		c = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.background },
 	},
 	insert = {
-		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.blue, gui = "bold" },
+		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.green, gui = "bold" },
 		b = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.line },
 		c = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.background },
 	},
 	visual = {
-		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.blue, gui = "bold" },
+		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.purple, gui = "bold" },
 		b = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.line },
 		c = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.background },
 	},
 	replace = {
-		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.blue, gui = "bold" },
+		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.red, gui = "bold" },
 		b = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.line },
 		c = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.background },
 	},
 	command = {
-		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.blue, gui = "bold" },
+		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.orange, gui = "bold" },
 		b = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.line },
 		c = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.background },
 	},
 	inactive = {
-		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.blue, gui = "bold" },
+		a = { fg = "#" .. config.colors.background, bg = "#" .. config.colors.line, gui = "bold" },
 		b = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.line },
 		c = { fg = "#" .. config.colors.foreground, bg = "#" .. config.colors.background },
 	},
