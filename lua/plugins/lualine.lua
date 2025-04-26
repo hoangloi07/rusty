fd1816b7c994d38c643df64a4456d3ecffe26b43
Arman Nikoyan <armannikoyan@Armans-MacBook-Air.local>
2025-04-26 13:02:37 +0400

    quick-fix: lualine colors not recognised properly

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,38 +1,46 @@
-local colors = require('rusty.colors').get()
+local rusty_colors = require('rusty.colors')
 
-require('lualine').setup({
-  options = {
-    theme = {
-      normal = {
-        a = { fg = colors.background, bg = colors.blue, gui = "bold" },
-        b = { fg = colors.foreground, bg = colors.line },
-        c = { fg = colors.foreground, bg = colors.background },
-      },
-      insert = {
-        a = { fg = colors.background, bg = colors.green, gui = "bold" },
-        b = { fg = colors.foreground, bg = colors.line },
-        c = { fg = colors.foreground, bg = colors.background },
-      },
-      visual = {
-        a = { fg = colors.background, bg = colors.purple, gui = "bold" },
-        b = { fg = colors.foreground, bg = colors.line },
-        c = { fg = colors.foreground, bg = colors.background },
-      },
-      replace = {
-        a = { fg = colors.background, bg = colors.red, gui = "bold" },
-        b = { fg = colors.foreground, bg = colors.line },
-        c = { fg = colors.foreground, bg = colors.background },
-      },
-      command = {
-        a = { fg = colors.background, bg = colors.orange, gui = "bold" },
-        b = { fg = colors.foreground, bg = colors.line },
-        c = { fg = colors.foreground, bg = colors.background },
-      },
-      inactive = {
-        a = { fg = colors.background, bg = colors.line, gui = "bold" },
-        b = { fg = colors.foreground, bg = colors.line },
-        c = { fg = colors.foreground, bg = colors.background },
-      },
-    },
-  },
-})
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'rusty' },
+  config = function()
+    local colors = rusty_colors.get()
+    require('lualine').setup({
+      options = {
+        theme = {
+          normal = {
+            a = { fg = colors.background, bg = colors.blue, gui = "bold" },
+            b = { fg = colors.foreground, bg = colors.line },
+            c = { fg = colors.foreground, bg = colors.background },
+          },
+          -- Add other mode definitions here
+          insert = {
+            a = { fg = colors.background, bg = colors.green, gui = "bold" },
+            b = { fg = colors.foreground, bg = colors.line },
+            c = { fg = colors.foreground, bg = colors.background },
+          },
+          visual = {
+            a = { fg = colors.background, bg = colors.purple, gui = "bold" },
+            b = { fg = colors.foreground, bg = colors.line },
+            c = { fg = colors.foreground, bg = colors.background },
+          },
+          replace = {
+            a = { fg = colors.background, bg = colors.red, gui = "bold" },
+            b = { fg = colors.foreground, bg = colors.line },
+            c = { fg = colors.foreground, bg = colors.background },
+          },
+          command = {
+            a = { fg = colors.background, bg = colors.orange, gui = "bold" },
+            b = { fg = colors.foreground, bg = colors.line },
+            c = { fg = colors.foreground, bg = colors.background },
+          },
+          inactive = {
+            a = { fg = colors.background, bg = colors.line, gui = "bold" },
+            b = { fg = colors.foreground, bg = colors.line },
+            c = { fg = colors.foreground, bg = colors.background },
+          },
+        }
+      }
+    })
+  end
+}
