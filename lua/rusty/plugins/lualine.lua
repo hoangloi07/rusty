c12907e635b5d2147d4e8fae9c8fdc9068ef8168
Arman Nikoyan <armannikoyan04@icloud.com>
2025-08-06 16:38:02 +0400

    fixed: lualine transparency

--- lua/rusty/plugins/lualine.lua
+++ lua/rusty/plugins/lualine.lua
@@ -11,27 +11,27 @@ return function(config)
     },
     insert = {
       a = { fg = colors.background, bg = colors.green, gui = "bold" },
-      b = { fg = colors.foreground, bg = colors.line },
+      b = { fg = colors.foreground, bg = colors.background },
       c = { fg = colors.foreground, bg = bg_color },
     },
     visual = {
       a = { fg = colors.background, bg = colors.purple, gui = "bold" },
-      b = { fg = colors.foreground, bg = colors.line },
+      b = { fg = colors.foreground, bg = colors.background },
       c = { fg = colors.foreground, bg = bg_color },
     },
     replace = {
       a = { fg = colors.background, bg = colors.red, gui = "bold" },
-      b = { fg = colors.foreground, bg = colors.line },
+      b = { fg = colors.foreground, bg = colors.background },
       c = { fg = colors.foreground, bg = bg_color },
     },
     command = {
       a = { fg = colors.background, bg = colors.orange, gui = "bold" },
-      b = { fg = colors.foreground, bg = colors.line },
+      b = { fg = colors.foreground, bg = colors.background },
       c = { fg = colors.foreground, bg = bg_color },
     },
     inactive = {
       a = { fg = colors.background, bg = colors.line, gui = "bold" },
-      b = { fg = colors.foreground, bg = colors.line },
+      b = { fg = colors.foreground, bg = colors.background },
       c = { fg = colors.foreground, bg = bg_color },
     },
   }
